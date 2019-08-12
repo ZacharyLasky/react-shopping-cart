a0908ed9f20b1e6eb4e08b3ded92ebc727acb9b3
Zach Lasky <zacharylasky123@gmail.com>
2019-08-12 16:36:20 -0400

    imported useContext in shoppingcart component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -18,13 +19,13 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation />
 			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
 			{/* Routes */}
-				<Route exact path="/" component={products}/>
-
-				<Route
-					path="/cart" render={() => <ShoppingCart cart={cart} />}/>
+				<Route exact path="/" component={Products}/>
+				<Route path="/cart" component={ShoppingCart}/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
